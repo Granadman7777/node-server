98a21a7662c652ece4e690c169af998cbefdce97
Grandman7777 <kokaiya7777@gmail.com>
2023-01-12 19:12:18 +0200

    Local-Host

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const MyMath =  require('./utils/MyMath');
+const http = require('http');
 
-console.log(MyMath.sum(6,9));
+const PORT = 3000;
+
+const requestListener = (req, res) => {
+   // console.log('Request is Here')
+   const {url, method} = req;
+   console.log(url, method);
+}
+
+const server = http.createServer(requestListener);
+
+server.listen(PORT);
