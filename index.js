8f9657ad3afcf948e50dd2bfe6f96b2b2e651120
Grandman7777 <kokaiya7777@gmail.com>
2023-01-12 20:24:36 +0200

    First-Server

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const http = require('http');
+const fs = require('fs/promises')
 
 const PORT = 3000;
 
 const requestListener = (req, res) => {
-   //const {url, method} = req;
-   //console.log(url, method);
+  const {url} = req;
+
+  if(url === '/index.html' ) {
+    fs.readFile('./vievs/index.html', 'utf-8') 
+    .then((data) => {
+        res.end(data)
+    })
+  } else {
+    res.statusCode = 404;
+    fs.readFile('./vievs/error.html', 'utf-8') 
+    .then((data) => {
+        res.end(data)
+    })
+  }
+
    res.statusCode = 404;
    res.end('HELLO SERVER');
 }
