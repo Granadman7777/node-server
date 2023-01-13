b23bf5b93fdbd8302a46a73ee4923e95d9317324
Grandman7777 <kokaiya7777@gmail.com>
2023-01-13 19:33:25 +0200

    Express

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello , My Friend');
-})
+app.get('/', (req, res, next) => {
+  console.log('first');
+  req.newfield = 'super-important-value'
+  next();
+}, (req, res, next) => {
+   console.log('second');
+   next();
+}, (req, res, next) => {
+   console.log(third);
+   console.log(req.newfield);
+});
 
 app.listen(PORT, () => {
   console.log(`Server Started on port ${PORT}`);
